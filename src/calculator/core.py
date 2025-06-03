a2be1e2c42c2b165c908ac4eaa75141b8f0f65c3
visnya_chern <73075390+vichnya@users.noreply.github.com>
2025-06-04 01:29:55 +0300

    Update core.py

--- src/calculator/core.py
+++ src/calculator/core.py
@@ -1,59 +1,123 @@
 import datetime
-import os
-import stat
+import math
+from tabulate import tabulate
 
-SETTINGS = {'op1': 1, 'op2': 1, 'op3': 1, 'op4': 1, 'op5': 1}
-PARAMS = {'precision': None, 'dest': None}
-
+PARAMS = {
+    'precision': None,
+    'output_type': None,
+    'possible_types': None,
+    'dest': None
+}
 
 def load(file="params.ini"):
+    """Перенос из текстового файла в словарь"""
     global PARAMS, SETTINGS
     with open(file, mode='r', errors='ignore') as f:
         lines = f.readlines()
-    for l in lines:
-        key, val = l.strip().split('=')
+
+    for line in lines:
+        key, val = line.strip().split('=')
         if key != 'dest':
             val = eval(val)
         if file == "params.ini":
             PARAMS[key] = val
-        elif file == "settings.ini":
-            SETTINGS[key] = val
 
+def write_log(*args, action=None, result=None, file='calc-history.log.txt'):
+    """Запись истории в файл"""
+    with open(file, mode='a', errors='ignore') as f:
+        date = datetime.datetime.today()
+        args_lst = list(args)
+        if len(args_lst) == 1:
+            f.write(
+                f'Дата: {date.strftime("%d/%m/%Y")}    Время:{date.strftime("%H:%M:%S")} \n{action}: {args_lst[0]} = {result} \n\n')
+        else:
+            f.write(
+                f'Дата: {date.strftime("%d/%m/%Y")}    Время: {date.strftime("%H:%M:%S")} \nОперация: {args[0]} {action} {args[1]}  = {result} \n\n')
 
-def write_log(*args, action=None, result=None, file='calc-history.log.txt', full_blok=False):
-    filename = 'newoutput.txt'
-    filename2 = 'newoutput.txt.txt'
-    new_permissions = stat.S_ENFMT
+def print_results(*args, action=None, result=None):
+    """Красивый вывод через tabulate"""
+    lst = list(args)
+    if len(args) == 1:
+        t_value = [lst, [action], [result]]
+        t_description = ['Числа', 'Операция', 'Результат']
+    else:
+        t_value = [[lst[0]], [action], [lst[1]], [result]]
+        t_description = ['Число 1', 'Операция', 'Число 2', 'Результат']
+
+    d = {t_description[i]: t_value[i] for i in range(len(t_description))}
+    print(tabulate(d, headers="keys", tablefmt="pretty"))
+
+def convert_precision():
+    """Определение количества знаков после запятой"""
+    val = float(PARAMS['precision'])
+    # Ограничим число знаков после запятой: округлим и сравним
+    for i in range(16):
+        if round(val, i) == val:
+            return i
+    return 15  # максимум
+
+def product(args):
+    """Приведение к заданной точности"""
+    ndigits = convert_precision()
+    return round(args, ndigits)
+
+def Calculator(op1, op2, act):
+    """Калькулятор с действиями"""
+    if act == "+": r = op1 + op2
+    elif act == "-": r = op1 - op2
+    elif act == "*": r = op1 * op2
+    elif act == "/": r = op1 / op2 if op2 != 0 else "Деление на ноль не реализуется"
+    elif act == "^": r = "Возведение 0 в отрицательную степень не реализуется" if op1 == 0 and op2 < 0 else op1 ** op2
+    elif act == "//": r = op1 // op2
+    elif act == "%": r = op1 % op2
+    else: r = "Операция не распознана"
+
+    write_log(op1, op2, action=act, result=r)
+    print_results(op1, op2, action=act, result=r)
+    return
+
+def standard_deviation():
+    """Среднеквадратичное отклонение с заданной точностью"""
     try:
-        os.chmod(filename, new_permissions)
-        if full_blok:
-            os.chmod(filename2, new_permissions)
-    except FileNotFoundError:
-        pass  # игнорируем отсутствие файла
+        number_count = int(input("Введите количество числел: "))
+        if number_count <= 1:
+            print("Введите количество значений больше одного")
+            return
+        args_list = [float(input(f'Введите {i} число: ')) for i in range(1, number_count + 1)]
+    except ValueError:
+        print('Проверьте вводимые значение на правильность: вводить необходимо целые числа')
+        return
 
-    date = datetime.datetime.today()
-    try:
-        with open(file, mode='a', errors='ignore') as f:
-            f.write(f'Run program\n')
-            f.write(f'Date {date.strftime("%d/%m/%Y")}\n')
-            f.write(f'Time {date.strftime("%H/%M/%S")}\n')
-            f.write(f'Operation {action}: {args} = {result}\n')
-    except PermissionError:
-        file_new = file + '.txt'
-        with open(file_new, mode='a', errors='ignore') as backup_file:
-            backup_file.write(f"{action}: {args} = {result}\n")
+    x_mean = sum(args_list) / len(args_list)
+    s_d = math.sqrt(sum((y - x_mean) ** 2 for y in args_list) / len(args_list))
+    result = round(s_d, convert_precision(PARAMS['precision']))
 
-
-def calculate(**kwargs):
-    args = [arg for arg in kwargs.values()]
-    res = sum(args)
-    write_log(*args, action='sum', result=res)
-    print(f'Операция sum: {args} = {res} \nРезультат: {res}')
-    return res
-
+    write_log(args_list, action='Среднеквадратичное отклонение', result=result)
+    print_results(args_list, action='Среднеквадратичное отклонение', result=result)
 
 def main():
     load("params.ini")
-    load("settings.ini")
-    calculate(**SETTINGS)
-    print("\nДля изменения параметров суммирования измените значения в settings.ini\n")
+
+    print("""
+Калькулятор с действиями
+
+  '+' - Сложение;
+  '-' - Вычитание;
+  '*' - Умножение;
+  '/' - Деление;
+  '^' - Возведение в степень;
+  '//' - Деление без остатка;
+  '%' - Деление по модулю; 
+""")
+    try:
+        operand1 = int(input("Оператор 1: "))
+        operand2 = int(input("Операнд 2: "))
+        sign = input("Оператор: ")
+    except ValueError:
+        print('Проверьте вводимые значение на правильность: вводить необходимо числа')
+        return
+
+    Calculator(operand1, operand2, sign)
+    print("\nПоиск среднеквадратичного отклонения\n")
+    standard_deviation()
+
